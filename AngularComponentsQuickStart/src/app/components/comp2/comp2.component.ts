83c7c7334164389054c270a6385dc5ca53c58cc5
eduardo SANCHEZ <eduardo.sanchez@aori.mx>
2018-05-11 16:28:55 -0500

    comp2 completo

--- AngularComponentsQuickStart/src/app/components/comp2/comp2.component.ts
+++ AngularComponentsQuickStart/src/app/components/comp2/comp2.component.ts
@@ -7,6 +7,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Comp2Component implements OnInit {
 
+  html=`
+<div class="container">
+  <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" class="image">
+  <div class="overlay">
+    <div class="text">Hello World</div>
+  </div>
+</div>
+  `;
+  css=`
+  .container {
+    position: relative;
+    width: 20%;
+  }
+  
+  .image {
+    display: block;
+    width: 100%;
+    height: auto;
+  }
+  
+  .overlay {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: #008CBA;
+    overflow: hidden;
+    width: 0;
+    height: 100%;
+    transition: .5s ease;
+  }
+  
+  .container:hover .overlay {
+    width: 100%;
+  }
+  
+  .text {
+    color: white;
+    font-size: 20px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    white-space: nowrap;
+  }
+  `;
+
   constructor() { }
 
   ngOnInit() {
